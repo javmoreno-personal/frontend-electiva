38553063c454ba837758c809f43d2d019c339767
Javier Moreno <92137837+javmoreno-personal@users.noreply.github.com>
2023-11-14 06:39:49 +0000

    last opportunity

--- src/Estudiantes.js
+++ src/Estudiantes.js
@@ -1,36 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const fetchEstudiantes = async () => {
-  try {
-    const response = await axios.get('http://universidadelectiva2.somee.com/api/Estudiante/GetAll');
-    return response.data.DatosRespuesta;
-  } catch (error) {
-    console.error('Error al cargar estudiantes:', error);
-    throw error; // Rethrow the error to indicate that the data fetching failed
-  }
-};
 
 const Estudiantes = () => {
   const [estudiantes, setEstudiantes] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const estudiantesData = await fetchEstudiantes();
-        setEstudiantes(estudiantesData);
-      } catch (error) {
-        // Handle error, e.g., show an error message to the user
-        console.error('Error al cargar estudiantes:', error);
-      }
-    };
+  const fetchEstudiantes = async () => {
+    try {
+      const response = await axios.get('http://universidadelectiva2.somee.com/api/Estudiante/GetAll');
+      setEstudiantes(response.data.DatosRespuesta);
+    } catch (error) {
+      console.error('Error al cargar estudiantes:', error.message);
+    }
+  };
 
-    fetchData();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+  useEffect(() => {
+    // You can remove the auto-fetch from useEffect if you prefer
+    // and use the button to trigger the fetch manually.
+    // fetchEstudiantes();
+  }, []);
+
+  const showAllStudents = () => {
+    fetchEstudiantes();
+  };
 
   return (
     <div>
       <h1>Listado de Estudiantes</h1>
+      <button onClick={showAllStudents}>Mostrar Todos</button>
       <table>
         <thead>
           <tr>
@@ -38,10 +34,8 @@ const Estudiantes = () => {
             <th>Nombres</th>
             <th>Apellidos</th>
             <th>Edad</th>
-            <th>IdGenero</th>
             <th>Curso</th>
-            <th>Id</th>
-            <th>Activo</th>
+            {/* Add more headers based on your data */}
           </tr>
         </thead>
         <tbody>
@@ -51,10 +45,8 @@ const Estudiantes = () => {
               <td>{estudiante.Nombres}</td>
               <td>{estudiante.Apellidos}</td>
               <td>{estudiante.Edad}</td>
-              <td>{estudiante.IdGenero}</td>
               <td>{estudiante.Curso}</td>
-              <td>{estudiante.Id}</td>
-              <td>{estudiante.Activo.toString()}</td>
+              {/* Add more cells based on your data */}
             </tr>
           ))}
         </tbody>
