1d76f109881c95dff219619401fc73a2e1cc36c9
Javier Moreno <92137837+javmoreno-personal@users.noreply.github.com>
2023-11-14 05:58:43 +0000

    Fix consume student

--- src/Estudiantes.js
+++ src/Estudiantes.js
@@ -1,22 +1,28 @@
+
+// Estudiantes.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 // src/Estudiantes.js
 
 // ... (importaciones)
 
+const fetchEstudiantes = async () => {
+  try {
+    const response = await axios.get('http://universidadelectiva2.somee.com/api/Estudiante/GetAll');
+    return response.data;
+  } catch (error) {
+    console.error('Error al cargar estudiantes:', error);
+  }
+};
+
 const Estudiantes = () => {
   const [estudiantes, setEstudiantes] = useState([]);
 
-  useEffect(() => {
-    // Lógica para cargar estudiantes desde la API
-    const fetchEstudiantes = async () => {
-      try {
-        const response = await axios.get('http://universidadelectiva2.somee.com/api/Estudiante/GetAll');
-        setEstudiantes(response.data);
-      } catch (error) {
-        console.error('Error al cargar estudiantes:', error);
-      }
-    };
-
-    fetchEstudiantes();
+  useEffect(async () => {
+    // Cargar estudiantes desde la API
+    const estudiantes = await fetchEstudiantes();
+    setEstudiantes(estudiantes);
   }, []);
 
   return (
