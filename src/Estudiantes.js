8d461d7975df7d2f2d9bf1a449e117f05ceea2f9
Javier Moreno <92137837+javmoreno-personal@users.noreply.github.com>
2023-11-14 06:14:09 +0000

    New thing in student to create other parts

--- src/Estudiantes.js
+++ src/Estudiantes.js
@@ -1,12 +1,8 @@
-
-// Estudiantes.js
+// Import necessary dependencies
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// src/Estudiantes.js
-
-// ... (importaciones)
-
+// Function to fetch students from the API
 const fetchEstudiantes = async () => {
   try {
     const response = await axios.get('http://universidadelectiva2.somee.com/api/Estudiante/GetAll');
@@ -16,19 +12,23 @@ const fetchEstudiantes = async () => {
   }
 };
 
+// React component for displaying the list of students
 const Estudiantes = () => {
+  // State to store the list of students
   const [estudiantes, setEstudiantes] = useState([]);
 
+  // useEffect to fetch students when the component mounts
   useEffect(async () => {
     // Cargar estudiantes desde la API
-    const estudiantes = await fetchEstudiantes();
-    setEstudiantes(estudiantes);
-  }, []);
+    const estudiantesData = await fetchEstudiantes();
+    setEstudiantes(estudiantesData);
+  }, []); // Empty dependency array ensures the effect runs once when the component mounts
 
   return (
     <div>
       <h1>Listado de Estudiantes</h1>
       <ul>
+        {/* Map through the list of students and display their names */}
         {estudiantes.map((estudiante) => (
           <li key={estudiante.Id}>{estudiante.Nombres} {estudiante.Apellidos}</li>
         ))}
